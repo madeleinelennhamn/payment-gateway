252a06831186cc2448e38aba22d5ac449072c96e
madeleine.lennhamn <madeleine.lennhamn@gmail.com>
2021-09-08 08:18:42 +0200

    removed swagger

--- src/main/java/se/nackademin/java20/pgw/SpringFoxConfig.java
+++ src/main/java/se/nackademin/java20/pgw/SpringFoxConfig.java
@@ -6,15 +6,3 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-
-@Configuration
-public class SpringFoxConfig {
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
-    }
-}
