52f17da7fd82cb1b4d3a98c4c0949e3b399d02f3
madeleine.lennhamn <madeleine.lennhamn@gmail.com>
2021-09-08 08:24:01 +0200

    added annotation

--- src/main/java/se/nackademin/java20/pgw/PGWApplication.java
+++ src/main/java/se/nackademin/java20/pgw/PGWApplication.java
@@ -1,6 +1,9 @@
 package se.nackademin.java20.pgw;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class PGWApplication {
 
     public static void main(String[] args) {
